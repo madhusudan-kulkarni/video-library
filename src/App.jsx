bdcf99f4486cce97273dcfaa42ab817e14aa452f
Madhusudan Kulkarni <96856144+madhusudan-kulkarni@users.noreply.github.com>
2023-07-29 15:29:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen ">
+      <div className="text-4xl text-center">
+        <p>React Starter Template</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
